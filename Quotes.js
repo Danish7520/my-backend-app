6cf7d58d4d95a36245303df971982cd22817bc4f
Muhammad Danish Nawaz <danishlashari3@gmail.com>
2018-11-06 16:31:31 +0500

    Update Quotes.js

--- Quotes.js
+++ Quotes.js
@@ -29,5 +29,117 @@ module.exports = [
   {
     quote: "Take it easy driving– the life you save may be mine.",
     author: "James Dean"
+  },
+  {
+    quote: "The cars we drive say a lot about us.",
+    author: "Alexandra Paul"
+  },
+  {
+    quote: "We aren't addicted to oil, but our cars are.",
+    author: "James Woolsey"
+  },
+  {
+    quote:
+      "Self-driving cars are the natural extension of active safety and obviously something we should do.",
+    author: "Elon Musk"
+  },
+  {
+    quote: "Older cars tend to drive like older cars. That is not for me.",
+    author: "Robert Herjavec"
+  },
+  {
+    quote:
+      "When it comes to cars, only two varieties of people are possible - cowards and fools.",
+    author: "Russell Baker"
+  },
+  {
+    quote: "I love vintage cars because you can do so much more to them.",
+    author: "T-Pain"
+  },
+  {
+    quote:
+      "Electric cars are not going to take the market by storm, but it's going to be a gradual improvement.",
+    author: "Carlos Ghosn"
+  },
+  {
+    quote: "Fast cars are my only vice.",
+    author: "Michael Bay"
+  },
+  {
+    quote: "A racing car is an animal with a thousand adjustments.",
+    author: "Mario Andretti"
+  },
+  {
+    quote:
+      "Electric cars aren't pollution-free; they have to get their energy from somewhere.",
+    author: "Alexandra Paul"
+  },
+  {
+    quote: "Cars bring me sheer joy. ",
+    author: "Evan Spiegel"
+  },
+  {
+    quote: "Fast cars like Porsches and Ferraris - they are things of beauty.",
+    author: "Rod Stewart"
+  },
+  {
+    quote: "I couldn't find the sports car of my dreams, so I built it myself.",
+    author: "Ferdinand Porsche"
+  },
+
+  {
+    quote:
+      "A car is like a mother-in-law - if you let it, it will rule your life.",
+    author: "Jaime Lerner"
+  },
+  {
+    quote:
+      "Flying cars are not a very efficient way to move things from one point to another.",
+    author: "Bill Gates"
+  },
+  {
+    quote: "There's nothing wrong with the car except that it's on fire.",
+    author: "Murray Walker"
+  },
+  {
+    quote:
+      "I think about cars to try and distract myself. It's a good way to relax, take your mind off everything. ",
+    author: "Asafa Powell"
+  },
+  {
+    quote: "Never lend your car to anyone to whom you have given birth.",
+    author: "Erma Bombeck"
+  },
+  {
+    quote: "Cars are the sculptures of our everyday lives.",
+    author: "Chris Bangle"
+  },
+  {
+    quote: "Cars mean nothing to me. I'm not a car guy.",
+    author: "Penn Jillette"
+  },
+  {
+    quote:
+      "You can know or not know how a car runs and still enjoy riding in a car.",
+    author: "David Byrne"
+  },
+  {
+    quote: "Modern cars I don't like so much.",
+    author: "Amber Heard"
+  },
+  {
+    quote:
+      "Race cars, no matter what size or shape they are, they do the same things. It is not complicated.",
+    author: "Tony Stewart"
+  },
+  {
+    quote:
+      "Generally, cars were not built to sit on dealer lots. It encourages the wrong kind of behavior in the whole system.",
+    author: "Rick Wagoner"
+  },
+  {
+    quote:
+      "I've always been into cars. Cars are part of our genetic makeup. It's unavoidable.",
+    author: "Matthew Fox"
   }
 ];
